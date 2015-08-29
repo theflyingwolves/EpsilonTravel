b6aa38130a218300ecbf6082ac9447acaf5731a3
Wang Kunzhen <theflyingwolves@gmail.com>
2015-08-29 20:41:38 +0800

    Receipts Modal Added

--- EasyTripManagement/js/services.js
+++ EasyTripManagement/js/services.js
@@ -1,5 +1,6 @@
-angular.module("easytrip", [])
-.factory('Trips', function(){
+angular.module('easytrip.services',[])
+
+.service('Trips', function(){
 	var allTrips = [
 	{
 		"title":"Trip 1",
@@ -101,59 +102,12 @@ angular.module("easytrip", [])
 		},
 
 		select: function(item) {
-			console.log("Selecting "+JSON.stringify(item));
 			selected = item;
 		},
 
 		selected: function() {
-			console.log("Selected: "+JSON.stringify(selected));
 			return selected;
 		}
 	};
-})
 
-.controller("ReceiptCtrl", function($scope, Trips){
-	$scope.receiptList = Trips.selected().receipts;
-})
-
-.controller("MainCtrl", function($scope, Trips) {
-	$scope.filter = {};
-
-	$scope.isShown = function(trip) {
-		if (($scope.filter.title == undefined || $scope.filter.title.length == 0 || trip.title.toLowerCase().indexOf($scope.filter.title.toLowerCase()) >= 0 ) &&
-				($scope.filter.destination == undefined || $scope.filter.destination.length == 0 || trip.destination.toLowerCase().indexOf($scope.filter.destination.toLowerCase()) >= 0 ) &&
-				($scope.filter.participants  == undefined || $scope.filter.participants.length == 0 || trip.participants.toLowerCase().indexOf($scope.filter.participants.toLowerCase()) >= 0 ) &&
-				($scope.filter.start  == undefined || $scope.filter.start.length == 0 || trip.start.toLowerCase().indexOf($scope.filter.start.toLowerCase()) >= 0 ) &&
-				($scope.filter.end  == undefined || $scope.filter.end.length == 0 || trip.end.toLowerCase().indexOf($scope.filter.end.toLowerCase()) >= 0 ) &&
-				($scope.filter.summary  == undefined || $scope.filter.summary.length == 0 || trip.summary.toLowerCase().indexOf($scope.filter.summary.toLowerCase()) >= 0 ) && 
-				($scope.filter.expense  == undefined || $scope.filter.expense.length == 0 || trip.expense.toLowerCase().indexOf($scope.filter.expense.toLowerCase()) >= 0 )) {
-			return true;
-		} else {
-			return false;
-		}
-	};
-
-	$scope.totalExpense;
-
-	$scope.selectedTrip;
-
-	$scope.select = function(item){
-		$scope.selectedTrip = item;
-		Trips.select(item);
-	};
-
-	$scope.$watch('filter',function(){
-		var total = 0;
-
-		for(var index = 0; index < $scope.tripList.length; index++) {
-			var trip = $scope.tripList[index];
-			if ($scope.isShown(trip)) {
-				total += parseFloat(trip.expense);
-			}
-		}
-
-		$scope.totalExpense = total;
-	}, true);
-
-	$scope.tripList = Trips.all();
 });
