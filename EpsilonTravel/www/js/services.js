228c7164f41468b14b0bc12ab3b209046a0033ff
Tang Zijian <tangzj77@gmail.com>
2015-08-29 17:09:28 +0800

    commit

---
+++ EpsilonTravel/www/js/services.js
@@ -0,0 +1,58 @@
+angular.module('starter.services', [])
+
+.factory('ReceiptService', function() {
+	//mock data
+	var receipts = [
+	  {title: 'receipt1', imgUrl: '../img/receipts/receipt1.jpeg', description: 'taxi fee', date: 'October 13, 2014 11:13:00', price: 50, id: 'r1'},
+	  {title: 'receipt2', imgUrl: '../img/receipts/receipt2.jpeg', description: 'lunch', date: 'June 4, 2014 11:13:00', price: 100, id: 'r2'},
+	  {title: 'receipt3', imgUrl: '../img/receipts/receipt3.jpeg', description: 'hotel', date: 'May 23, 2014 11:13:00', price: 200, id: 'r3'},
+	  {title: 'receipt4', imgUrl: '../img/receipts/receipt4.jpeg', description: 'something', date: 'March 1, 2014 11:13:00', price: 300, id: 'r4'}
+	];
+
+	return {
+		all: function() {
+			return receipts;
+		},
+		add: function(receipt) {
+			receipts.push(receipt);
+		}, 
+		get: function(receiptId) {
+			for (var i = 0; i < receipts.length; i++) {
+				var r = receipts[i];
+				if (r.id === receiptId) {
+					return r;
+				}
+			}
+			return null;
+		},
+		update: function(receipt) {
+			for (var i = receipts.length - 1; i >= 0; i--) {
+				var r = receipts[i];
+				if (receipt.id === r.id) {
+					r.title = receipt.title;
+					r.imgUrl = receipt.imgUrl;
+					r.description = receipt.description;
+					r.date = receipt.date;
+					r.price = receipt.price;
+				}
+			}
+		}
+	};
+})
+
+.factory('CameraService', function($q) {
+	return {
+    getPicture: function(options) {
+      var q = $q.defer();
+
+      navigator.camera.getPicture(function(result) {
+        // Do any magic you need
+        q.resolve(result);
+      }, function(err) {
+        q.reject(err);
+      }, options);
+
+      return q.promise;
+    }
+  };
+});
